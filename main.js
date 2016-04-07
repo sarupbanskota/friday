3c7e3ba9b1963ace02929015b3e5fb0e9c9fdd26
Sarup Banskota <sbanskota08@gmail.com>
2016-04-07 21:54:48 +0800

    Check for friday and show image on friday instead

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 dayScore = {
-		"1": 4, 
-		"2": 3, 
-		"3": 2, 
-		"4": 1, 
-		"5": 0, 
-		"6": 6, 
-		"0": 5
-	}
+    "1": 4, 
+    "2": 3, 
+    "3": 2, 
+    "4": 1, 
+    "5": 0, 
+    "6": 6, 
+    "0": 5
+}
+
 today = new Date;
 today = today.getDay();
 fridayScore = ((6 - dayScore[today]) / 6).toFixed(2);
-console.log(fridayScore);
 
-var circle = new ProgressBar.Circle('#progressbar-container', {
-    color: '#FCB03C',
-    strokeWidth: 10,
-    trailWidth: 3,
-    duration: 1500,
-    text: {
-      value: '0'
-    },
-    step: function(state, bar) {
-      bar.setText("Today's Fridaaay score: " + fridayScore);
+if(fridayScore == 1){
+  var fridayMarkup = "<img src='images/friday.jpg'></img>";
+  container = document.getElementById('progressbar-container');
+  container.innerHTML += fridayMarkup;
+} else{
+
+  var circle = new ProgressBar.Circle('#progressbar-container', {
+      strokeWidth: 10,
+      trailWidth: 3,
+      duration: 1500,
+      text: {
+        value: '0'
+      },
+      step: function(state, bar) {
+        bar.setText("Today's Fridaaay score: " + fridayScore);
+      }
+  });
+  circle.animate(fridayScore,
+    {
+      from: { color: '#eee' },
+      to: { color: '#000' },
     }
-});
-
-circle.animate(fridayScore);
+  );
+}
